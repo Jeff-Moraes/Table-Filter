a08b93c1209c868ee7121632ec4ebd53aa632c21
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-11-04 21:12:52 +0100

    Table Page: created initial table structure

--- src/pages/Table/index.jsx
+++ src/pages/Table/index.jsx
@@ -4,28 +4,62 @@ import getTableData from '../../lib/getTableData';
 
 function Table({ user }) {
   const [ allData, setAllData ] = useState(null);
-  const [ tableDataKeys, setTableDataKeys ] = useState(null);
+  const [ tableData, setTableData ] = useState(null);
+
+  const [ tablePage, setTablePage ] = useState(1);
+  const [ numberOfResults, setNumberOfResults ] = useState(10);
 
   const fetchTableData = async () => {
-
     const dataFromTableData = await getTableData();
   
     const valuesFromTableData = Object.values(dataFromTableData);
-    const keysFromTableData = Object.keys(dataFromTableData[1]);
-  
     setAllData(valuesFromTableData);
-    setTableDataKeys(keysFromTableData);
+
+    const intialTableData = valuesFromTableData.slice(0,numberOfResults);
+    setTableData(intialTableData);
   }
 
   useEffect(() => {
     fetchTableData();
   }, []);
   
+  console.log(tableData)
   return (
     <div>
-      <h1>Table {user.username}</h1>
+      <h1>Table</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>image</th>
+            <th>product name</th>
+            <th>product desc</th>
+            <th>category</th>
+            <th>quantity</th>
+            <th>style</th>
+            <th>color</th>
+            <th>price</th>
+            <th>currency</th>
+          </tr>
+        </thead>
+        <tbody>
+          { tableData && tableData.map(tableData => (
+            <tr key={tableData.id}>
+              <td><img src={tableData.image} alt={tableData.product_name}/></td>
+              <td>{tableData.product_name}</td>
+              <td>{tableData.product_desc}</td>
+              <td>{tableData.price}</td>
+              <td>{tableData.currency}</td>
+              <td>{tableData.category}</td>
+              <td>{tableData.quantity}</td>
+              <td>{tableData.style}</td>
+              <td>{tableData.color}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
 
-export default Table
+export default Table;
