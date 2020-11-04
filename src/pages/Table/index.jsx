c975a21517728b8fdd8da2874eee823cf5cb19fc
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-11-04 23:40:00 +0100

    Table Page: created input to search product by name

--- src/pages/Table/index.jsx
+++ src/pages/Table/index.jsx
@@ -9,8 +9,11 @@ function Table() {
   const [ allData, setAllData ] = useState(null);
   const [ tableData, setTableData ] = useState(null);
 
+  const [ filteredData, setFilteredData ] = useState(null);
   const [ tablePage, setTablePage ] = useState(1);
   const [ numberOfResults, setNumberOfResults ] = useState(10);
+
+  const [ productNameToSearch, setProductNameToSearch ] = useState("");
 
   const fetchTableData = async () => {
     const dataFromTableData = await getTableData();
@@ -20,6 +23,7 @@ function Table() {
 
     const intialTableData = await valuesFromTableData.slice(0,numberOfResults);
     setTableData(intialTableData);
+    setFilteredData(intialTableData);
   }
 
   const handlePreviousPage = () => {
@@ -28,29 +32,53 @@ function Table() {
   }
 
   const handleNextPage = () => {
-    if(tablePage === allData.length / numberOfResults) return;
+    if(tablePage === Math.ceil(filteredData.length / numberOfResults)) return;
     setTablePage(tablePage+1);
   }
 
-  const updateTableData = () => {
-    const newTableData = allData?.slice((tablePage - 1) * numberOfResults, (tablePage * numberOfResults));
-    if(newTableData) setTableData(newTableData);
+  const filterAllDataByProductName = async () => {
+    const filteredAllDataByProductName = await allData?.filter(product => product.product_name.includes(productNameToSearch));
+    setFilteredData(filteredAllDataByProductName);
+    setTablePage(1);
   }
-
+  
+  const updateTableData = () => {
+    const newTableData = filteredData?.slice((tablePage - 1) * numberOfResults, (tablePage * numberOfResults));
+    setTableData(newTableData);
+  }
+  
   useEffect(() => {
     fetchTableData();
   }, []);
-
+  
   useEffect(() => {
     updateTableData();
-  }, [tablePage, numberOfResults]);
+  }, [tablePage, numberOfResults, filteredData]);
+  
+  useEffect(() => {
+    filterAllDataByProductName();
+  }, [productNameToSearch]);
   
   return (
     <div>
       <h1>Table</h1>
       <form>
         <label htmlFor="numberOfResults">Select the number of results per page</label>
-        <input type="number" name="numberOfResults" id="numberOfResults" onChange={(event) => setNumberOfResults(event.target.value)}/>
+        <input
+          type="number"
+          name="numberOfResults"
+          id="numberOfResults"
+          value={numberOfResults}
+          onChange={(event) => setNumberOfResults(event.target.value)}
+        />
+        <label htmlFor="numberOfResults">Search a product by name</label>
+        <input
+          type="text"
+          name="productNameToSearch"
+          id="productNameToSearch"
+          value={productNameToSearch}
+          onChange={(event) => setProductNameToSearch(event.target.value)}
+        />
       </form>
 
       <table>
@@ -66,7 +94,7 @@ function Table() {
 
       <button type="button" onClick={handlePreviousPage} disabled={tablePage === 1}>previous</button>
       <span>page {tablePage}</span>
-      <button type="button" onClick={handleNextPage}>next</button>
+      <button type="button" onClick={handleNextPage} disabled={tablePage === Math.ceil(filteredData?.length / numberOfResults)}>next</button>
     </div>
   )
 }
