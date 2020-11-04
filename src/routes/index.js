edbcad8f8a4f14d3406504ab31613ca51fc18e58
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-11-04 16:32:54 +0100

    Routes: created basic routes

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import Login from "../pages/Login";
+import Table from "../pages/Table";
+
+const Routes = (props) => {
+  const [user, setUser] = useState(props.user);
+  return (
+    <Switch>
+      <Route path="/" exact component={Login} />
+      <Route
+        path="/table"
+        render={(props) => {
+          if (user) {
+            return <Table {...props} user={user} setUser={setUser} />;
+          } else {
+            return <Redirect to="/" />;
+          }
+        }}
+      />
+    </Switch>
+  );
+};
+
+export default Routes;
