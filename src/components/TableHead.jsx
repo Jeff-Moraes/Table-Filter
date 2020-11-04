a5db8c1fd0245a8b0b63446017eab74610a9e112
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-11-04 21:15:05 +0100

    TableHead: created TableHead component

---
+++ src/components/TableHead.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function TableHead() {
+  return (
+    <thead>
+      <tr>
+        <th>image</th>
+        <th>product name</th>
+        <th>product desc</th>
+        <th>category</th>
+        <th>quantity</th>
+        <th>style</th>
+        <th>color</th>
+        <th>price</th>
+        <th>currency</th>
+      </tr>
+    </thead>
+  )
+}
