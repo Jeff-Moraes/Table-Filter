fd08d373ed43bba35c91a93613ef031ad42d367c
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-11-05 12:24:56 +0100

    PageButtons: added bootstrap class

--- src/components/PageButtons/index.jsx
+++ src/components/PageButtons/index.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 
 export default function PageButtons({ tablePage, handlePreviousPage, handleNextPage, lastPageNumber }) {
   return (
-    <div>
+    <div className="d-flex justify-content-center align-items-center mt-5 mb-5">
       <button 
+        className="btn btn-outline-secondary"
         type="button"
         onClick={handlePreviousPage}
         disabled={tablePage === 1}>
         previous
       </button>
 
-      <span>page {tablePage}</span>
+      <span className="mr-3 ml-3">page {tablePage}</span>
 
-      <button 
+      <button
+        className="btn btn-outline-secondary"
         type="button"
         onClick={handleNextPage}
         disabled={tablePage === lastPageNumber}>
