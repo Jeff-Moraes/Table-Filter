2a6aa445d998a80481c023600907346c0b5f948a
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-11-05 10:53:23 +0100

    Style: created GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
+import GlobalStyle from "./styles/global";
 import Routes from "./routes";
 
 const App = () => (
@@ -8,6 +9,7 @@ const App = () => (
     <BrowserRouter>
       <Routes />
     </BrowserRouter>
+    <GlobalStyle />
   </>
 );
 
