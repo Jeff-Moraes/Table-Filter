c0329b17e88173725edc7c87ca71d7819cb0a58c
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-11-05 12:16:53 +0100

    Search: added basic form and inputs style

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,32 +1,48 @@
 import React from 'react'
 
+import { SearchContainer } from './styles';
+
 export default function Search({ numberOfResults, setNumberOfResults, productNameToSearch, setProductNameToSearch, colorOptions, setSelectedColor }) {
   return (
-    <form>
-      <label htmlFor="numberOfResults">Select the number of results per page</label>
-      <input
-        type="number"
-        name="numberOfResults"
-        id="numberOfResults"
-        value={numberOfResults}
-        onChange={(event) => setNumberOfResults(event.target.value)}
-      />
-      <label htmlFor="numberOfResults">Search a product by name</label>
-      <input
-        type="text"
-        name="productNameToSearch"
-        id="productNameToSearch"
-        value={productNameToSearch}
-        onChange={(event) => setProductNameToSearch(event.target.value)}
-      />
-      { colorOptions && (
-        <select name="colorOptions" id="colorOptions" onChange={(event) => setSelectedColor(event.target.value)}>
-          <option value="">Select color</option>
-          { colorOptions.map(color => (
-            <option key={color} value={color}>{color}</option>
-          ))}
-        </select>
-      )}
-    </form>
+    <SearchContainer className="mb-3 mt-3">
+      <div className="productName">
+        <input
+          className="form-control"
+          placeholder="Search by product name"
+          type="text"
+          name="productNameToSearch"
+          id="productNameToSearch"
+          value={productNameToSearch}
+          onChange={(event) => setProductNameToSearch(event.target.value)}
+        />
+      </div>
+      <div>
+        { colorOptions && (
+          <select
+            className="custom-select colorOptions"
+            name="colorOptions"
+            id="colorOptions" 
+            onChange={(event) => setSelectedColor(event.target.value)}
+          >
+            <option value="">Select color</option>
+            { colorOptions.map(color => (
+              <option key={color} value={color}>{color}</option>
+            ))}
+          </select>
+        )}
+      </div>
+      <div className="input-group numberOfResults">
+        <input
+          type="number"
+          name="numberOfResults"
+          id="numberOfResults"
+          value={numberOfResults}
+          onChange={(event) => setNumberOfResults(event.target.value)}
+        />
+        <div className="input-group-append">
+          <span className="input-group-text" id="basic-addon2">results per page</span>
+        </div>
+      </div>
+    </SearchContainer>
   )
 }
