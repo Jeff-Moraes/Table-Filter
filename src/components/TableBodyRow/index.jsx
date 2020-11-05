b6ed831f198290f6e7b39ff15706815f2a970cf4
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-11-05 09:14:12 +0100

    TableBodyRow: fixed td order

--- src/components/TableBodyRow/index.jsx
+++ src/components/TableBodyRow/index.jsx
@@ -6,12 +6,12 @@ export default function TableBodyRow({ tableData }) {
       <td><img src={tableData.image} alt={tableData.product_name}/></td>
       <td>{tableData.product_name}</td>
       <td>{tableData.product_desc}</td>
-      <td>{tableData.price}</td>
-      <td>{tableData.currency}</td>
       <td>{tableData.category}</td>
       <td>{tableData.quantity}</td>
       <td>{tableData.style}</td>
       <td>{tableData.color}</td>
+      <td>{tableData.price}</td>
+      <td>{tableData.currency}</td>
     </tr>
   )
 }
