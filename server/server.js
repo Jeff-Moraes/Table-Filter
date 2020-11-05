1135a0f5787131fd5fb3039328125270bfb31d6a
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-11-05 21:27:50 +0100

    Server: configured mongodb database

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const bodyParser= require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+MongoClient.connect('mongodb+srv://tablefilter:tablefilter@cluster0.wtjse.mongodb.net/tablefilter?retryWrites=true&w=majority', {
+    useUnifiedTopology: true })
+.then(client => {
+    console.log('Connected to Database')
+})
+.catch(error => console.error(error))
 
 app.listen(5555, function() {
   console.log('listening on 5555');
