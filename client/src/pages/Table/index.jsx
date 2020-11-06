e9fc02c99be5f2fb15145b81e93e22dbe348b347
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-11-06 01:16:41 +0100

    Table Page: refactored - fetch the data from API

--- client/src/pages/Table/index.jsx
+++ client/src/pages/Table/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
-
-import getTableData from '../../lib/getTableData';
+import axios from 'axios';
 
 import Search from '../../components/Search';
 import TableHead from '../../components/TableHead';
@@ -8,29 +7,22 @@ import TableBodyRow from '../../components/TableBodyRow';
 import PageButtons from '../../components/PageButtons';
 
 function Table() {
-  const [ allData, setAllData ] = useState(null);
-  const [ tableData, setTableData ] = useState(null);
   const [ filteredData, setFilteredData ] = useState(null);
-
   const [ colorOptions, setColorOptions ] = useState(null);
 
   const [ tablePage, setTablePage ] = useState(1);
   const [ selectedColor, setSelectedColor ] = useState("");
   const [ numberOfResults, setNumberOfResults ] = useState(10);
-  const [ productNameToSearch, setProductNameToSearch ] = useState("");
+  const [ searchProducts, setSearchProducts ] = useState("");
 
-  const fetchTableData = async () => {
-    const dataFromTableData = await getTableData();
-  
-    const valuesFromTableData = await Object.values(dataFromTableData);
-    setAllData(valuesFromTableData);  
+  const fetchAllDataFromAPI = async () => {
+    const { data } = await axios.get(`http://localhost:5555/search?limit=${numberOfResults}&page=${tablePage}&searchProducts=${searchProducts}&selectedColor=${selectedColor}`);
+    setFilteredData(data)
+  }
 
-    const colorOptionsFromTableData = [...new Set(valuesFromTableData.map(product => product.color))].filter(color => color);
-    setColorOptions(colorOptionsFromTableData);
-
-    const intialTableData = valuesFromTableData.slice(0,numberOfResults);
-    setTableData(intialTableData);
-    setFilteredData(valuesFromTableData);
+  const fetchColorsFromAPI = async () => {
+    const { data } = await axios.get('http://localhost:5555/colors');
+    setColorOptions(data)
   }
 
   const handlePreviousPage = () => {
@@ -42,48 +34,32 @@ function Table() {
     if(tablePage === Math.ceil(filteredData.length / numberOfResults)) return;
     setTablePage(tablePage+1);
   }
-
-  const filterAllData = () => {
-    const filteredAllDataByColor = selectedColor ? allData?.filter(product => product.color === selectedColor) : allData;
-    const filteredAllDataByProductName = filteredAllDataByColor?.filter(product => product.product_name.includes(productNameToSearch));
-    setFilteredData(filteredAllDataByProductName);
-    setTablePage(1);
-  }
-  
-  const updateTableData = () => {
-    const newTableData = filteredData?.slice((tablePage - 1) * numberOfResults, (tablePage * numberOfResults));
-    setTableData(newTableData);
-  }
   
   useEffect(() => {
-    fetchTableData();
+    fetchAllDataFromAPI();
+    fetchColorsFromAPI();
   }, []);
   
   useEffect(() => {
-    updateTableData();
-  }, [tablePage, numberOfResults, filteredData]);
-  
-  useEffect(() => {
-    filterAllData();
-  }, [productNameToSearch, selectedColor]);
+    fetchAllDataFromAPI();
+  }, [tablePage, numberOfResults, searchProducts, selectedColor]);
 
   return (
     <div className="py-5 px-4">
       <h1 className="display-3">Table Filter</h1>
 
       <Search
+        colorOptions={colorOptions}
         numberOfResults={numberOfResults}
         setNumberOfResults={setNumberOfResults}
-        productNameToSearch={productNameToSearch}
-        setProductNameToSearch={setProductNameToSearch}
-        colorOptions={colorOptions}
+        setSearchProducts={setSearchProducts}
         setSelectedColor={setSelectedColor}
       />
       
       <table className="table">
         <TableHead />
         <tbody>
-          { tableData ? tableData.map(tableData => (
+          { filteredData ? filteredData.map(tableData => (
             <TableBodyRow key={tableData.id} tableData={tableData} />
           )) : (
             <tr><td>Loading...</td></tr>
